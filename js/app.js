890366ba2c5ecfb4de6cfb2232380e5d35a734c0
AlphaCrewDev4 <carlos.g@client-access.com>
2022-02-10 17:02:27 -0800

    added footer section

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 
-// Variables
-const menuToggle = document.querySelector('#menu-toggle');
-const menu = document.querySelector('.custom-nav-list');
+(() => {
+    // Variables
+    const menuToggle = document.querySelector('#menu-toggle');
+    const menu = document.querySelector('.custom-nav-list');
+    const customYear = document.querySelector('#custom-year');
+    const date = new Date();
+    const year = date.getFullYear();
+    customYear.innerHTML = year;
 
 
-menuToggle.addEventListener('click', ()=> {
-    console.log(menu);
-    if(menuToggle.checked === true) {
-        $('.custom-nav-list').slideToggle('slow','swing', function() {
-            menu.classList.add('show-menu');
-        } );
-        
-        
-    } else {
-        $('.custom-nav-list').slideToggle('slow','swing', function() {
-            menu.classList.remove('show-menu');
-        } );
-        
-    }
-});
+    menuToggle.addEventListener('click', () => {
+        console.log(menu);
+        if (menuToggle.checked === true) {
+            $('.custom-nav-list').slideToggle('slow', 'swing', function () {
+                menu.classList.add('show-menu');
+            });
+
+
+        } else {
+            $('.custom-nav-list').slideToggle('slow', 'swing', function () {
+                menu.classList.remove('show-menu');
+            });
+
+        }
+    });
+})();
