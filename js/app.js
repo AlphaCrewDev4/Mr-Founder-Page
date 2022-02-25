8599514bf605e82d6ccb907a90b8ea9ac750beb7
AlphaCrewDev4 <carlos.g@client-access.com>
2022-02-25 10:28:49 -0800

    new index page design

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@
     const menuToggle = document.querySelector('#menu-toggle');
     const menu = document.querySelector('.custom-nav-list');
     const customYear = document.querySelector('#custom-year');
+    const header = document.querySelector('.custom-nav');
+
+
+    // Get year for footer
     const date = new Date();
     const year = date.getFullYear();
     customYear.innerHTML = year;
@@ -28,11 +32,18 @@
     //AOS function
     AOS.init();
 
+    // Sticky Menu
+
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('sticky', window.scrollY > 50);
+    });
+
     //Card Carrousel
     $('.custom-slider').owlCarousel({
         loop: true,
         autoplay: true,
-        autoplayTimeout: 2000,
+        autoplayTimeout: 4500,
+        smartSpeed: 6000,
         autoplayHoverPause: true,
         responsive: {
             0: {
